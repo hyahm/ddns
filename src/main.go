081cb0d4e53a42cb45b7d8c285775c27243f115c
cander <cander@cander.com>
2019-01-30 14:24:56 +0800

    ddns bug

--- src/main.go
+++ src/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"galog"
+	"gamail"
 	"gassh"
 	"io/ioutil"
 	"log"
@@ -16,6 +18,10 @@ var RemoteFile string
 var AfterExec string
 
 func main() {
+	//gaconfig.InitConf("ddns")
+
+	galog.DefInitLogger("log",0,false)
+
 	afterExec := flag.String("a", "nginx -s reload", "reload http server command")
 	interval := flag.Int("t", 60, "interval time, default 60 Second")
 	remoteIp := flag.String("r", "", "server ip")
@@ -24,6 +30,12 @@ func main() {
 	username := flag.String("u", "root", "ssh username")
 	password := flag.String("p", "", "ssh password")
 	keyfile := flag.String("k", "~/.ssh/id_rsa", "ssh private key")
+	defaultip := flag.String("d", "", "default ip")
+
+	emailuser := flag.String("eu","","email username")
+	emailpwd := flag.String("ep","","email password")
+	emailport := flag.Int("eo",0,"email username")
+	emailto := flag.String("et","","email to who")
 	flag.Parse()
 	if *remoteIp == "" {
 		log.Fatal("server ip must be need, use -r serverip")
@@ -31,12 +43,17 @@ func main() {
 	if *remoteFile == "" {
 		log.Fatal("remotefile ip must be need, ps: -f /usr/local/nginx/conf/vhost/aa.conf")
 	}
+	if *defaultip == "" {
+		log.Fatalf("defaultip ip must be need in %s",*remoteFile)
+	}
+	IP = *defaultip
 	RemoteIp = *remoteIp
 	AfterExec = *afterExec
 	RemoteFile = *remoteFile
 	for {
 		resp, err := http.Get("http://ip.hyahm.com")
 		if err != nil {
+			galog.Error(err.Error())
 			time.Sleep(time.Second * 3)
 			continue
 		}
@@ -44,11 +61,22 @@ func main() {
 		s = s[:len(s)-1]
 
 		if string(s) != IP {
-			if *password != "" {
-				execShellWithPassword(*username, *password, *port, string(s))
-			} else {
-				execShellWithKey(*username, *keyfile, *port, string(s))
+			if *emailuser != "" && *emailpwd != "" && *emailport != 0 && *emailto != "" {
+				email(*emailuser,*emailpwd ,*emailport,string(s),*emailto)
 			}
+
+			if *password != "" {
+				err = execShellWithPassword(*username, *password, *port, string(s))
+				if err != nil {
+					email(*emailuser,*emailpwd ,*emailport,"update fail",*emailto)
+				}
+			} else {
+				err = execShellWithKey(*username, *keyfile, *port, string(s))
+				if err != nil {
+					email(*emailuser,*emailpwd ,*emailport,"update fail",*emailto)
+				}
+			}
+			email(*emailuser,*emailpwd ,*emailport,"update success",*emailto)
 			IP = string(s)
 		}
 		fmt.Println(string(s))
@@ -61,6 +89,7 @@ func execShellWithPassword(username string, password string, port int, newip str
 	sconf := gassh.Password(username, password)
 	sshconn, err := sconf.Connect(fmt.Sprintf("%s:%d", RemoteIp, port))
 	if err != nil {
+		galog.Error(err.Error())
 		return err
 	}
 	defer sshconn.Close()
@@ -70,10 +99,13 @@ func execShellWithPassword(username string, password string, port int, newip str
 
 func execShellWithKey(username string, key string, port int, newip string) error {
 	sconf := gassh.PrivateKey(username, key)
+	// 连接远程机器
 	sshconn, err := sconf.Connect(fmt.Sprintf("%s:%d", RemoteIp, port))
 	if err != nil {
+		galog.Error(err.Error())
 		return err
 	}
+	fmt.Println("远程连接成功")
 	defer sshconn.Close()
 	err = exec(sshconn, newip)
 	if err != nil {
@@ -84,8 +116,9 @@ func execShellWithKey(username string, key string, port int, newip string) error
 
 func exec(conn *gassh.SshConn, newip string) error {
 
-	_, err := conn.ExecShell(fmt.Sprintf(` sed -i "s/%s/%s/g`, IP, newip))
+	_, err := conn.ExecShell(fmt.Sprintf(" sed -i 's/%s/%s/g' %s", IP, newip,RemoteFile))
 	if err != nil {
+		fmt.Println(err)
 		return err
 	}
 	fmt.Println("change success")
@@ -95,4 +128,15 @@ func exec(conn *gassh.SshConn, newip string) error {
 	}
 	fmt.Println("reload service success")
 	return nil
+}
+
+func email(username string,password string,port int,content string, to string) {
+	econf := gamail.Newmailconfig()
+	econf.Username = username
+	econf.Password = password
+	econf.Subject = "ip changed"
+	econf.Content = content
+	econf.Port = port
+	econf.Tolist = []string{to}
+	econf.SendMail()
 }
